1b3e3c208ccd6af888601378c30e4ce0c4f953ee
ouaZAK <zakariaouaraqa@example.com>
2023-07-03 18:44:08 +0100

    start

---
+++ ex01/header.hpp
@@ -0,0 +1,26 @@
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#include <iostream>
+
+class PhoneBook {
+	private :
+		Contact contacts[8];
+	
+	public :
+
+};
+
+class Contact {
+	private :
+		std::string first_name[11];
+		std::string last_name[11];
+		std::string nickname[11];
+		std::string phone_number[11];
+		std::string darkest_secret[11];
+
+	public :
+	
+};
+
+#endif
