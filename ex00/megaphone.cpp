6091efc7f271a59737433e136a6eafe52e5baec3
ouaZAK <zakariaouaraqa@example.com>
2023-07-03 18:02:41 +0100

    good

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,20 +2,17 @@
 
 int main(int ac, char **av)
 {
-	(void)av;
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		for (int i = 1; i < ac; i++)
-		{
 			for (int j = 0; av[i][j]; j++)
 			{
 				if (av[i][j] >= 'a' && av[i][j] <= 'z')
 					av[i][j] -= 32;
 				std::cout << av[i][j];
 			}
-		}
 	}
 	std::cout << std::endl;
 	return (0);
