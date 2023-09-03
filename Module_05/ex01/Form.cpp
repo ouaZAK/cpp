8e1a977f3051a5496cf2e8cedf01e37adb7b7e02
ouaZAK <zakariaouaraqa@gmail.com>
2023-09-03 15:17:19 +0100

    modifie mod5 ex01

--- Module_05/ex01/Form.cpp
+++ Module_05/ex01/Form.cpp
@@ -6,9 +6,72 @@
 /*   By: zouaraqa <zouaraqa@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 13:19:04 by zouaraqa          #+#    #+#             */
-/*   Updated: 2023/09/03 13:19:49 by zouaraqa         ###   ########.fr       */
+/*   Updated: 2023/09/03 15:07:15 by zouaraqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
+Form::Form() : name("none"), sign_grade(0), exec_grade(0), signe(false)
+{
+	std::cout << "default constructor called" << std::endl;
+}
+
+Form::Form(std::string name, const int sg, const int eg) : name(name), sign_grade(sg), exec_grade(eg)
+{
+	std::cout << "custom constructor called" << std::endl;
+}
+
+Form::Form(const Form &form) : name(form.name), sign_grade(form.sign_grade), exec_grade(form.exec_grade)
+{
+	std::cout << "copy constructor called" << std::endl;
+}
+
+Form &Form::operator = (const Form &form)
+{
+	//what to do
+	return (*this);
+}
+
+Form::~Form()
+{
+	std::cout << "destructor called" << std::endl;
+}
+
+const std::string	Form::getName(void) const
+{
+	return (name);
+}
+
+const int	Form::getSignGrade(void) const
+{
+	return (sign_grade);
+}
+const int	Form::getExecGrade(void) const
+{
+	return (exec_grade);
+}
+
+bool	Form::getSigne(void) const
+{
+	return (signe);
+}
+
+const char *Form::GradeTooHighException::what() const throw()
+{
+	return ("Form::GradeTooHighException");
+}
+
+const char *Form::GradeTooLowException::what() const throw()
+{
+	return ("Form::GradeTooLowException");
+}
+
+std::ostream &operator << (std::ostream &out, const Form &form)
+{
+	out << "name : " << form.getName() << "\n"
+		<< "signed grade : " << form.getSignGrade() << "\n"
+		<< "executed : " << form.getExecGrade() << "\n"
+		<< "signed : " << form.getSigne() << std::endl;
+	return (out);
+}
--- Module_05/ex01/Form.hpp
+++ Module_05/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: zouaraqa <zouaraqa@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 13:19:08 by zouaraqa          #+#    #+#             */
-/*   Updated: 2023/09/03 13:51:55 by zouaraqa         ###   ########.fr       */
+/*   Updated: 2023/09/03 15:02:45 by zouaraqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,15 @@ class Form
 		const int			exec_grade;
 		bool				signe;
 	public:
-		const std::string	getName(void);
-		const int			getSignGrade(void);
-		const int			getSignGrade(void);
-		bool				getSigne(void);
+		Form();
+		Form(const std::string name, const int sg, const int eg);
+		Form(const Form &form);
+		Form	&operator = (const Form &from);
+		const std::string	getName(void) const;
+		const int			getSignGrade(void) const;
+		const int			getExecGrade(void) const;
+		bool				getSigne(void) const;
+		
 		class GradeTooHighException
 		{
 			public:
@@ -38,3 +43,5 @@ class Form
 				const char *what() const throw();
 		};
 };
+
+std::ostream &operator << (std::ostream &out, const Form &form);
